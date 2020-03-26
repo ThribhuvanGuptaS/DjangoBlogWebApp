81182628045c9532fcf93a18c7f97671a5281b16
ThribhuvanGuptaS <thribhuvangupta@gmail.com>
2020-03-26 19:36:35 +0530

    fixed registeration page error

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages # to include message.debug, message.info, message.success, message.warning, message.error
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-#from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -10,29 +10,33 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Hey {username} ! Your account has been created successfully! You can now login!') # one time flash message [ f''(f-string) supported in python 3.6.x+ only]
-            return redirect('login') # after user registration redirect to home page 
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated') # one time flash message [ f''(f-string) supported in python 3.6.x+ only]
-            return redirect('profile') # after user registration redirect to home page 
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-    
+
     context = {
-        'u_form' : u_form,
-        'p_form' : p_form
+        'u_form': u_form,
+        'p_form': p_form
     }
 
     return render(request, 'users/profile.html', context)
