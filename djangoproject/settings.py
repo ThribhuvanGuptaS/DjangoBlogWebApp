e5a894a7239c7e23bc6f1ad1865967a81e7ad60c
ThribhuvanGuptaS <thribhuvangupta@gmail.com>
2020-03-26 19:55:46 +0530

    minor fix

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'thribhuvangupta@gmail.com'
-EMAIL_HOST_PASSWORD = 'smnsrnezfhrdarju'
+EMAIL_HOST_USER = # add email id here
+EMAIL_HOST_PASSWORD = # add pass here
